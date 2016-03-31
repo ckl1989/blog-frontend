7765ce3f873742960dd02da1693d08bcbeb8df2a
Chris Lam <christopher.kong.lam@gmail.com>
2016-03-30 18:30:02 -0700

    checkin in ember part of blog project

---
+++ app/routes/post/comment/new.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model(){
+		return {};
+	},
+	renderTemplate(){
+		this.render('post.comment.new', {into: 'application'});
+	},
+	actions:{
+		save(){
+			const post = this.modelFor('post');
+			const newComment = this.store.createRecord('comment', this.currentModel);
+			newComment.set('post', post);
+			newComment.save().then(() => {this.transitionTo('post', post)
+			});
+		},
+		cancel(){
+			this.transitionTo('post', post);
+		}
+	}
+});
